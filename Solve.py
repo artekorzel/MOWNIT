0a58940e1e7fd84046e2dc5a2c8cfd8978b25ba7
Artur Orzeł <artekorzel@gmail.com>
2012-05-30 17:13:54 +0200

    2nd initial commmit

--- Solve.py
+++ Solve.py
@@ -1,5 +1,10 @@
 from subprocess import Popen,PIPE
 import sys
+from os import environ
+
+env = dict(environ)
+env['LD_LIBRARY_PATH'] = '.'
+
 s1=r'''option solver "./donlp2";
 model "./Model.mod";
 data "'''
@@ -18,9 +23,10 @@ printf "\n";
 '''
 def python_solver(file_name):
 	global s1,s2
+	global env
 	#with open('./amplscript', 'r') as p:
 		#out=Popen(['./ampl'],stdout=PIPE,stdin=p).communicate()
-	out=Popen(['./ampl'],stdout=PIPE,stdin=PIPE).communicate(s1+file_name+s2)
+	out=Popen(['./ampl'],stdout=PIPE,stdin=PIPE,env=env).communicate(s1+file_name+s2)
 	return out[0].split('@')[1]
 
 if __name__=='__main__':

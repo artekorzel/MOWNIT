cf43675f5a0965fa4851cac0f916ecaa29c887a1
adebski <adebski@student.agh.edu.pl>
2012-06-14 09:16:23 +0200

    Modified Creator script to include starting gnuplot

--- Creator.py
+++ Creator.py
@@ -20,28 +20,29 @@ from java.awt import GridLayout
 from javax.swing import JTable
 from javax.swing import JScrollPane
 from javax.swing import JOptionPane
-import gen,re,Solve,Grapher
+import gen, re, Solve, Grapher
+from subprocess import Popen,PIPE
 users = []
 apps = []
 drives = []
 
 class EmptyFieldException(Exception):
-    def __init__(self,value):
-        self.value=value
+    def __init__(self, value):
+        self.value = value
         
     def __str__(self):
         return self.value
 
 def Convert(level):
-    if level=='low':
+    if level == 'low':
         return '1'
-    elif level=='medium':
+    elif level == 'medium':
         return '2'
-    elif level=='high':
+    elif level == 'high':
         return '3'
-    elif level=='remote':
+    elif level == 'remote':
         return '0'
-    elif level=='direct':
+    elif level == 'direct':
         return '1'
 
 class StaticContent(object):
@@ -50,19 +51,19 @@ set user_params := profile read write security access priority;
 set drive_params := read_s write_s size;\n'''
     userHeaders = ['id', 'profile', 'read', 'write', 'security', 'access', 'priority']
     userProfiles = ['1', '2']
-    appsHeaders = ['user','name' ,'read B/s', 'generated B/s', 'time']
-    drivesHeaders = ['id','read speed', 'write speed', 'size']
+    appsHeaders = ['user', 'name' , 'read B/s', 'generated B/s', 'time']
+    drivesHeaders = ['id', 'read speed', 'write speed', 'size']
     levelNames = ['low', 'medium', 'high']
     accessTypes = ['remote', 'direct']
-    param_apps='''param applications : 
+    param_apps = '''param applications : 
           user read gen time prior:=\n'''
-    param_drives='''param phys_drives :
+    param_drives = '''param phys_drives :
           read_s write_s size := \n'''
-    param_users='''param sys_users :
+    param_users = '''param sys_users :
           profile read write security access priority := \n'''
-    apps=[]
-    drives=[]
-    users=[]
+    apps = []
+    drives = []
+    users = []
     
 
 def deb(event):
@@ -75,8 +76,8 @@ def contextChange(event):
 
 class Creator(object):
     def __init__(self):
-        self.initialized=0
-        self.tPane = JTabbedPane(JTabbedPane.TOP,stateChanged=self.updateUserInfo)     
+        self.initialized = 0
+        self.tPane = JTabbedPane(JTabbedPane.TOP, stateChanged=self.updateUserInfo)     
         #setting user panel
         self.createUserTab(self.tPane)
         #setting apps panel
@@ -88,7 +89,7 @@ class Creator(object):
         self.window.add(self.tPane)
         self.window.visible = 1
         self.window.pack()
-        self.initialized=1
+        self.initialized = 1
     
     def createMenus(self):
         menuBar = JMenuBar()
@@ -106,7 +107,7 @@ class Creator(object):
         #self.userTable.setAutoCreateRowSorter(1)
         #setting cell editors
         column = self.userTable.getColumnModel().getColumn(0) #user column
-        column.setCellEditor(DefaultCellEditor(JTextField(),editingStopped=self.userNameEdited));
+        column.setCellEditor(DefaultCellEditor(JTextField(), editingStopped=self.userNameEdited));
         column = self.userTable.getColumnModel().getColumn(2) #read column
         column.setCellEditor(DefaultCellEditor(JTextField(editable=0)));
         column = self.userTable.getColumnModel().getColumn(3) #write column
@@ -134,7 +135,7 @@ class Creator(object):
         self.appsTable.getModel().removeRow(0)
         #self.appsTable.setAutoCreateRowSorter(1)
         #setting cell editors
-        self.userBox=JComboBox(users)
+        self.userBox = JComboBox(users)
         column = self.appsTable.getColumnModel().getColumn(0) #profile column
         column.setCellEditor(DefaultCellEditor(self.userBox));
         appsPanel.add(JScrollPane(self.appsTable)
@@ -158,61 +159,61 @@ class Creator(object):
         drivesPanel.add(buttonPanel, BorderLayout.SOUTH)
         
     def saveData(self, fileName):
-        StaticContent.apps=[]
-        StaticContent.drives=[]
-        StaticContent.users=[]
+        StaticContent.apps = []
+        StaticContent.drives = []
+        StaticContent.users = []
         
         f = open(fileName, "w+b")
-        userModel=self.userTable.getModel()
-        appsModel=self.appsTable.getModel()
-        drivesModel=self.drivesTable.getModel()
-        user_num=1
-        apps_set='set apps := '
-        drives_set='set drives := '
-        users_set='set users := '
-        apps_table=[]
-        users_table=[]
-        drives_table=[]
+        userModel = self.userTable.getModel()
+        appsModel = self.appsTable.getModel()
+        drivesModel = self.drivesTable.getModel()
+        user_num = 1
+        apps_set = 'set apps := '
+        drives_set = 'set drives := '
+        users_set = 'set users := '
+        apps_table = []
+        users_table = []
+        drives_table = []
         try:        
             #fetching users and info about them
-            rows=userModel.getRowCount()
+            rows = userModel.getRowCount()
             for row in range(rows):
-                user=userModel.getValueAt(row,0)
-                if user is None or re.match('\w',user) is None:
+                user = userModel.getValueAt(row, 0)
+                if user is None or re.match('\w', user) is None:
                     raise EmptyFieldException('Empty user name')
-                users_set+=(' '+user)
-                priority=Convert(userModel.getValueAt(row,6))
-                app_rows=appsModel.getRowCount()
+                users_set += (' ' + user)
+                priority = Convert(userModel.getValueAt(row, 6))
+                app_rows = appsModel.getRowCount()
                 for app_row in range(app_rows):
-                    app_user=appsModel.getValueAt(app_row,0)
+                    app_user = appsModel.getValueAt(app_row, 0)
                     if app_user == user:
-                        name=appsModel.getValueAt(app_row,1)
-                        read_sec=appsModel.getValueAt(app_row,2)
-                        write_sec=appsModel.getValueAt(app_row,3)
-                        time=appsModel.getValueAt(app_row,4)
-                        apps_set+=(' '+name)
-                        apps_table.append('%*s %-*s %-*s %-*s %-*s %-s\n' % (10,name,4,str(user_num),4,read_sec,3,write_sec,4,time,priority))
-                        StaticContent.apps.append(gen.app(name,user_num,write_sec,read_sec,time,priority))
-                profile=userModel.getValueAt(row,1)
-                read=userModel.getValueAt(row,2)
-                write=userModel.getValueAt(row,3)
-                security=Convert(userModel.getValueAt(row,4))
-                access=Convert(userModel.getValueAt(row,5))
-                users_table.append('%*s %-*s %-*s %-*s %-*s %-*s %-s\n' % (10,user,7,profile,4,read,5,write,8,security,6,access,priority))
-                StaticContent.users.append(gen.user(user,profile,read,write,security,access,priority))
-                user_num+=1
-            users_set+=';\n'
-            apps_set+=';\n'
-            rows=drivesModel.getRowCount()
+                        name = appsModel.getValueAt(app_row, 1)
+                        read_sec = appsModel.getValueAt(app_row, 2)
+                        write_sec = appsModel.getValueAt(app_row, 3)
+                        time = appsModel.getValueAt(app_row, 4)
+                        apps_set += (' ' + name)
+                        apps_table.append('%*s %-*s %-*s %-*s %-*s %-s\n' % (10, name, 4, str(user_num), 4, read_sec, 3, write_sec, 4, time, priority))
+                        StaticContent.apps.append(gen.app(name, user_num, write_sec, read_sec, time, priority))
+                profile = userModel.getValueAt(row, 1)
+                read = userModel.getValueAt(row, 2)
+                write = userModel.getValueAt(row, 3)
+                security = Convert(userModel.getValueAt(row, 4))
+                access = Convert(userModel.getValueAt(row, 5))
+                users_table.append('%*s %-*s %-*s %-*s %-*s %-*s %-s\n' % (10, user, 7, profile, 4, read, 5, write, 8, security, 6, access, priority))
+                StaticContent.users.append(gen.user(user, profile, read, write, security, access, priority))
+                user_num += 1
+            users_set += ';\n'
+            apps_set += ';\n'
+            rows = drivesModel.getRowCount()
             for row in range(rows):
-                name=drivesModel.getValueAt(row,0)
-                drives_set+=' '+name
-                read_s=drivesModel.getValueAt(row,1)
-                write_s=drivesModel.getValueAt(row,2)
-                size=drivesModel.getValueAt(row,3)
-                drives_table.append('%*s %-*s %-*s %-s\n' % (10,name,6,read_s,7,write_s,size))
-                StaticContent.drives.append(gen.drive(name,read_s,write_s,size))
-            drives_set+=';\n'
+                name = drivesModel.getValueAt(row, 0)
+                drives_set += ' ' + name
+                read_s = drivesModel.getValueAt(row, 1)
+                write_s = drivesModel.getValueAt(row, 2)
+                size = drivesModel.getValueAt(row, 3)
+                drives_table.append('%*s %-*s %-*s %-s\n' % (10, name, 6, read_s, 7, write_s, size))
+                StaticContent.drives.append(gen.drive(name, read_s, write_s, size))
+            drives_set += ';\n'
             f.write(apps_set)
             f.write(users_set)
             f.write(drives_set)
@@ -230,91 +231,120 @@ class Creator(object):
                 f.write(drv)
             f.write(';\n')
         except EmptyFieldException, e:
-            JOptionPane.showMessageDialog(None,"One of the fields is empty - "+str(e),"Empty field\n",JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(None, "One of the fields is empty - " + str(e), "Empty field\n", JOptionPane.ERROR_MESSAGE);
         #except Exception, e2:
         #    JOptionPane.showMessageDialog(None,"There was an error during writing - "+str(e2),"Error",JOptionPane.ERROR_MESSAGE);
         f.close()
         
         
-    def solveData(self,event):
+    def solveData(self, event):
         fileChooser = JFileChooser()
         fileChooser.showSaveDialog(None)
         out = fileChooser.selectedFile
         self.saveData(out.toString())
-        grapherData=Solve.parser(Solve.python_solver(out.toString()))
-        print grapherData
-        tupla=(StaticContent.apps,StaticContent.drives,StaticContent.users,grapherData)
-        #tupla=([gen.app('a',1,11,11,11,0)],[gen.drive('a',11,11,121)],[],[[1.0]])
-        Grapher.graph(tupla)
+        #grapherData = Solve.parser(Solve.python_solver(out.toString()))
+        grapherData = Solve.parser(Solve.python_solver(out.toString()))
+        #print grapherData
+        #creating temporary file for usual Python script
+        apps=StaticContent.apps
+        drives=StaticContent.drives
+        users=StaticContent.users
+        #apps = [gen.app('z', 1, 11, 20, 20, 5),gen.app('z2', 1, 11, 15, 15, 4)]
+       # drives = [gen.drive('a', 100, 100, 100),gen.drive('b',60,60,60)]
+        #grapherData=[[1.0]]
+        i = 0
+        for drive in drives:
+            mult = drives[i].size
+            j = 0
+            for app in apps:
+                grapherData[i][j] *= float(mult)
+                j += 1
+            i += 1
+        #created graph-ready array of data, writing to temporary file
+        f = open("plotData", "w")
+        #grapherData=[[2.0,3.5],[1.0,3.0]]
+        i_range = len(grapherData)
+        j_range = len(grapherData[0])
+        for i in range(i_range):
+            for j in range(j_range):
+                    f.write(str(float(i)) + ' ' + str(float(j)) + ' ' + str(grapherData[i][j]) + '\n')
+                    f.write(str(float(i)) + ' ' + str(float(j + 1)) + ' ' + str(grapherData[i][j]) + '\n')
+            f.write('\n')
+            for j in range(j_range):
+                    f.write(str(float(i+1)) + ' ' + str(float(j)) + ' ' + str(grapherData[i][j]) + '\n')
+                    f.write(str(float(i+1)) + ' ' + str(float(j + 1)) + ' ' + str(grapherData[i][j]) + '\n')
+            f.write('\n')
+        f.close()
+        out=Popen(['gnuplot','-persist','gnuplot_script'],stdout=PIPE).communicate()
         
     
     def mousceClick(self, event):
-        column=self.userTable.getSelectedColumn()
+        column = self.userTable.getSelectedColumn()
         if (column == 0):
-            row=self.userTable.getSelectedRow()
-            self.tempUserName=self.userTable.getModel().getValueAt(self.userTable.convertRowIndexToModel(row),
+            row = self.userTable.getSelectedRow()
+            self.tempUserName = self.userTable.getModel().getValueAt(self.userTable.convertRowIndexToModel(row),
                                                                    self.userTable.convertColumnIndexToModel(column))
             
         
     def addData(self, event):
         if (event.actionCommand == 'Add user'):
-            self.userTable.getModel().insertRow(0, ['','1','0','0','low','direct','low'])
+            self.userTable.getModel().insertRow(0, ['', '1', '0', '0', 'low', 'direct', 'low'])
         elif (event.actionCommand == 'Add application'):
             self.appsTable.getModel().insertRow(0, [])
         elif (event.actionCommand == 'Add drive'):
             self.drivesTable.getModel().insertRow(0, [])
     def removeData(self, event):
         if (event.actionCommand == 'Remove user'):
-            column=self.userTable.getSelectedColumn()
-            row=self.userTable.getSelectedRow()
-            selectedUser=self.userTable.getModel().getValueAt(self.userTable.convertRowIndexToModel(row),
+            column = self.userTable.getSelectedColumn()
+            row = self.userTable.getSelectedRow()
+            selectedUser = self.userTable.getModel().getValueAt(self.userTable.convertRowIndexToModel(row),
                                                 self.userTable.convertColumnIndexToModel(column))
             self.userBox.removeItem(selectedUser)
             self.userTable.getModel().removeRow(self.userTable.convertRowIndexToModel(row))
         elif(event.actionCommand == 'Remove application'):
-            row=self.appsTable.getSelectedRow()
+            row = self.appsTable.getSelectedRow()
             self.appsTable.getModel().removeRow(self.appsTable.convertRowIndexToModel(row))
         elif(event.actionCommand == 'Remove drive'):
-            row=self.drivesTable.getSelectedRow()
+            row = self.drivesTable.getSelectedRow()
             self.drivesTable.getModel().removeRow(self.drivesTable.convertRowIndexToModel(row))
         
     #fired when there is an user id is changed        
     def userNameEdited(self, event):
         self.userBox.removeItem(self.tempUserName)
-        column=self.userTable.getSelectedColumn()
-        row=self.userTable.getSelectedRow()
-        newValue=self.userTable.getModel().getValueAt(self.userTable.convertRowIndexToModel(row),
+        column = self.userTable.getSelectedColumn()
+        row = self.userTable.getSelectedRow()
+        newValue = self.userTable.getModel().getValueAt(self.userTable.convertRowIndexToModel(row),
                                                 self.userTable.convertColumnIndexToModel(column))
         self.userBox.addItem(newValue)
     #we are refreshing the info about the user
-    def updateUserInfo(self,event):
-        tab=self.tPane.getSelectedIndex()
-        writeMap={}
-        readMap={}
+    def updateUserInfo(self, event):
+        tab = self.tPane.getSelectedIndex()
+        writeMap = {}
+        readMap = {}
         #must update user read/write info
-        if self.initialized==1 and tab==0:
-            rows=self.appsTable.getModel().getRowCount()
+        if self.initialized == 1 and tab == 0:
+            rows = self.appsTable.getModel().getRowCount()
             for row in range(rows):
-                user=self.appsTable.getModel().getValueAt(row,0) #we have user id - hopefully it is not empty
-                read=self.appsTable.getModel().getValueAt(row,2)
-                gen=self.appsTable.getModel().getValueAt(row,3)
-                time=self.appsTable.getModel().getValueAt(row,4)
+                user = self.appsTable.getModel().getValueAt(row, 0) #we have user id - hopefully it is not empty
+                read = self.appsTable.getModel().getValueAt(row, 2)
+                gen = self.appsTable.getModel().getValueAt(row, 3)
+                time = self.appsTable.getModel().getValueAt(row, 4)
                 if user is not None and gen is not None and read is not None and time is not None:
-                    if writeMap.get(user)==None:
+                    if writeMap.get(user) == None:
                         writeMap[user] = 0
-                    writeMap[user]+=int(gen)*int(time)
-                    if readMap.get(user)==None:
+                    writeMap[user] += int(gen) * int(time)
+                    if readMap.get(user) == None:
                         readMap[user] = 0
-                    readMap[user]+=int(read)*int(time)
-            rows=self.userTable.getModel().getRowCount()
+                    readMap[user] += int(read) * int(time)
+            rows = self.userTable.getModel().getRowCount()
             for row in range(rows):
-                user=self.userTable.getModel().getValueAt(row,0)
+                user = self.userTable.getModel().getValueAt(row, 0)
                 if writeMap.get(user) != None and readMap.get(user) != None:
-                    self.userTable.getModel().setValueAt(str(readMap[user]),row,2)
-                    self.userTable.getModel().setValueAt(str(writeMap[user]),row,3)
+                    self.userTable.getModel().setValueAt(str(readMap[user]), row, 2)
+                    self.userTable.getModel().setValueAt(str(writeMap[user]), row, 3)
                 else:
-                    self.userTable.getModel().setValueAt('0',row,2)
-                    self.userTable.getModel().setValueAt('0',row,3)
+                    self.userTable.getModel().setValueAt('0', row, 2)
+                    self.userTable.getModel().setValueAt('0', row, 3)
                     
 if __name__ == "__main__":    
     c = Creator()
